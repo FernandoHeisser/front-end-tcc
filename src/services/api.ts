8dcf006cc7cdf8f9d856eac196f4d4483bcbffac
FernandoHeisser <fernando.heisserch@gmai.com>
2022-06-26 16:54:44 -0300

    heroku url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/'
+    baseURL: 'https://heroku-tcc-back-end.herokuapp.com/'
+    //#baseURL: 'http://127.0.0.1:5000/'
 })
 
 export default api;
