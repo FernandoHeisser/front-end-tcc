320df06c02f2d32a23165e1d8b5ef039eed1a90d
FernandoHeisser <fernando.heisserch@gmai.com>
2022-06-30 21:11:26 -0300

    disable node_modules cache

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://heroku-tcc-back-end.herokuapp.com/'
-    //#baseURL: 'http://127.0.0.1:5000/'
+    //baseURL: 'https://heroku-tcc-back-end.herokuapp.com/'
+    baseURL: 'http://127.0.0.1:5000/'
 })
 
 export default api;
